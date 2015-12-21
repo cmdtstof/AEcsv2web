b6b5357312b94efa7f2f86d6cf26db7c5dbfc093
stof999cmdt <stof999@cmdt.ch>
2015-12-21 16:39:26 +0100

    getopt added

--- AppEnergie.pm
+++ AppEnergie.pm
@@ -7,23 +7,51 @@
 # create db, tables in www, charts, pdfs
 #
 #
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 
 package AppEnergie;
 use warnings;
 use strict;
 use Data::Dumper;
 
+use Getopt::Long;
+
 ##############################################################################
-# Define some constants
+# Define some constants / defaults
 #
 
-#my $ae_case = "appenergie";
 my $ae_case = "test";
-
+#my $ae_case = "appenergie";
 
 our $ae_dataStatus 		= "20.03.2015";    	# date of the db dump
+#TODO set dataStatus from last date in db 
 our $ae_writeLog 		= 1;				#1=write logfile and entries
-our $ae_stderrOutput 	= 1;				# 1=logfile output will also be sended to stderr
+our $ae_stderrOutput 	= 1; 				# 1=logfile output will also be sended to stderr
 our $ae_logDir			= "../log/";
 our $ae_logfile = $AppEnergie::ae_logDir . "logfile.csv";
 our $ae_outputDir; # outputdir csv, pdf, ... 
@@ -31,26 +59,6 @@ our $ae_wwwDataDir; #upload dir for data
 our $ae_db; # db dir
 our $ae_dbImportDumps; # sto csv for ae_importCsv
 our $ae_rawDataDir;	#sto raw data for $ae_importRaw
-
-if ($ae_case eq "appenergie") {
-	$ae_outputDir   	= "/data/bodies/appenergie/website/www_new/data/";
-	$ae_wwwDataDir		= 'sysadmin@websrv01.eleph.ch:/var/www/appenergie/data';
-	$ae_db			= "/data/bodies/appenergie/daten/db/sqlite/appenergie.db";		
-	$ae_dbImportDumps	= "/data/bodies/appenergie/daten/db/";
-	$ae_rawDataDir = "/data/bodies/appenergie/daten/raw/";
-	
-} elsif ($ae_case eq "test") {
-	$ae_outputDir   	= "../testdata/output/";
-	$ae_wwwDataDir		= "../testdata/www";	
-	$ae_db			= "../testdata/db/test.db";
-	$ae_dbImportDumps	= "../testdata/dumps/";
-	$ae_rawDataDir = "../testdata/raw/";	
-	
-} #else error!
-
-
-
-
 
 
 our $ae_ressDir			= "res/";		#ressourcen files (images, ....)
@@ -73,12 +81,48 @@ our $sep_char			= ";";
 my $ae_createDb 		= 0;    #1=create db
 my $ae_importDumps		= 0;	#1=import db dumps from csv (1.version) >>> create db !!!!
 
-my $ae_importRaw		= 0;	#1=import raw data into db
+my $ae_importRaw		= 1;	#1=import raw data into db
 my $ae_prodCsv			= 0;	# 1=create csv files
 my $ae_prodTbl			= 0;	# 1=produce tables
 #my $ae_prodCharts		= 0;	# 1=produce charts
 #my $ae_prodPdf			= 0;	# 1=produce pdf
-my $ae_uploadFiles		= 1;	# 1=upload files to fileserver
+my $ae_uploadFiles		= 0;	# 1=upload files to fileserver
+
+
+############# get options ##################
+GetOptions (
+	"profile|p=s" => \$ae_case, 
+	"verbose!" => \$ae_stderrOutput,
+	"createdb" => \$ae_createDb,
+	"importdumps" => \$ae_importDumps,
+	"importraw" => \$ae_importRaw,
+	"csv" => \$ae_prodCsv,
+	"tbl" => \$ae_prodTbl,
+	"upload" => \$ae_uploadFiles
+);
+
+
+############# get profile data ##################
+
+if ($ae_case eq "appenergie") {
+	$ae_outputDir   	= "/data/bodies/appenergie/website/www_new/data/";
+	$ae_wwwDataDir		= 'sysadmin@websrv01.eleph.ch:/var/www/appenergie/data';
+	$ae_db			= "/data/bodies/appenergie/daten/db/sqlite/appenergie.db";		
+	$ae_dbImportDumps	= "/data/bodies/appenergie/daten/db/";
+	$ae_rawDataDir = "/data/bodies/appenergie/daten/raw/";
+	
+} elsif ($ae_case eq "test") {
+	$ae_outputDir   	= "../testdata/output/";
+	$ae_wwwDataDir		= "../testdata/www";	
+	$ae_db			= "../testdata/db/test.db";
+	$ae_dbImportDumps	= "../testdata/dumps/";
+	$ae_rawDataDir = "../testdata/raw/";	
+	
+} else {  #else error!
+	die("Error wrong profile\n");
+}
+
+
 
 
 ############# main functions ##################
---
+++ appenergie.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# start script for AppEnergie case appenergie
+
+perl /data/bodies/appenergie/scr/scr/AppEnergie.pm \
+--profile appenergie \
+--importraw \
+--csv \
+--tbl \
+--upload
+
+
