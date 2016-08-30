8154da96ab3a5f88fce2b12ff8a8f5563a2434d7
stof999cmdt <stof999@cmdt.ch>
2016-08-30 16:51:59 +0200

    ready for github

--- AppEnergie.pm
+++ AppEnergie.pm
@@ -1,13 +1,18 @@
 #!/usr/bin/perl -Ilib 
 
-################################################
-# Appenzeller-Energie.ch
-# aeDataProcessor 
+##AEcsv2web##############################################
+# Appenzeller-Energie.ch DataProcessor 
 #
 # create db, csv, tables for www from raw leistungsdata
 #
-# uses different profiles (ae_case): test, appenergie (local and rsync to webserver), webserver (direct on webserver)
+# uses different profiles (ae_case):
+# - test
+# - appenergie (local processing and rsync to webserver)
+# - webserver (processing on webserver)
 #
+# info (ät) cmdt.ch
+#
+
 
 package AppEnergie;
 use warnings;
---
+++ README.md
@@ -0,0 +1,12 @@
+##AEcsv2web##############################################
+# Appenzeller-Energie.ch DataProcessor 
+#
+# create db, csv, tables for www from raw leistungsdata
+#
+# uses different profiles (ae_case):
+# - test
+# - appenergie (local processing and rsync to webserver)
+# - webserver (processing on webserver)
+#
+# info (ät) cmdt.ch
+#
