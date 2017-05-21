ac57e8b5e993e866fe2115a6b2707be759cda5f3
stof999cmdt <stof999@cmdt.ch>
2017-05-21 15:57:02 +0200

    add symlinked files

--- lib/Utili/Datum.pm
+++ lib/Utili/Datum.pm
@@ -1,1 +1,99 @@
-/data/code/cmdt_perl_tools/lib/Utili/Datum.pm
+#!/usr/bin/perl
+
+package Utili::Datum;
+
+use warnings;
+use strict;
+
+#use Data::Dumper;
+
+my $sep = "-";
+
+sub dateRawToDb {
+	my ($rawDate) = @_; #DD.MM.YYYY
+	
+	#without quotes!!!!
+
+	my $tag = substr($rawDate, 0, 2);
+	my $monat = substr($rawDate, 3, 2);
+	my $jahr = substr($rawDate, 6, 4);
+	
+	my $dbDate = $jahr . $sep . $monat . $sep . $tag; #YYYY-MM-DD
+	return $dbDate;
+	
+}
+
+sub subtractDateWithMonth {
+	my ($dateIn, $subMonth) = @_; #YYYY-MM-DD
+	my $dateOut;
+	if (defined $dateIn) {
+		my $jahr = substr($dateIn, 0, 4);
+		my $monat = substr($dateIn, 5, 2);
+		my $tag = substr($dateIn, 8, 2);
+		
+		my $newMonat = $monat - $subMonth;
+		if ($newMonat < 1) {
+			$newMonat = $monat + 12 - $subMonth;
+			$jahr = $jahr - 1;
+		}
+		$newMonat = sprintf("%02d", $newMonat);
+		$dateOut = $jahr . $sep . $newMonat . $sep . $tag; #YYYY-MM-DD
+	}
+	return $dateOut;
+
+}
+
+sub getSqliteTimestamp {
+	my ($sec,$min,$hour,$day,$month,$year,@rest) =   getLocaltimeFormated();
+	return ($year."-".$month."-".$day." ".$hour.":".$min.":".$sec);	
+	
+}
+
+sub getYYYYMMDD {
+	my ($sec,$min,$hour,$day,$month,$year,@rest) =   getLocaltimeFormated();
+	return ($year . $month . $day);
+}
+
+sub getYYYYMMDDHHMMSS {
+	my ($sec,$min,$hour,$day,$month,$year,@rest) =   getLocaltimeFormated();
+	return ($year . $month . $day . $hour . $min . $sec);
+	
+}
+
+sub getLocaltimeFormated {
+	my ($sec,$min,$hour,$day,$month,$year,@rest) = getLocaltime();
+	($sec,$min,$hour,$day,$month,$year,@rest) = localtimeFormater($sec,$min,$hour,$day,$month,$year,@rest);
+	return ($sec,$min,$hour,$day,$month,$year, @rest); 
+}
+
+sub getLocaltime {
+	my ($sec,$min,$hour,$day,$month,$year,@rest) =   localtime(time);#######To get the localtime of your system
+	return ($sec,$min,$hour,$day,$month,$year,@rest);
+}
+
+sub getPosix2HumanFormStr{
+	my ($dtPosix) = @_; 	#YYYYMMDDHHMMSS
+	my ($sec,$min,$hour,$day,$month,$year, @rest) = getPosix2HumanFormatet($dtPosix);
+	return "$year-$month-$day $hour:$min:$sec"; 
+}
+
+sub getPosix2HumanFormatet{
+	my ($dtPosix) = @_;
+	my ($sec,$min,$hour,$day,$month,$year,@rest) =   localtime($dtPosix);
+	($sec,$min,$hour,$day,$month,$year,@rest) = localtimeFormater($sec,$min,$hour,$day,$month,$year,@rest);		
+	return ($sec,$min,$hour,$day,$month,$year, @rest);
+}
+
+sub localtimeFormater{
+	my ($sec,$min,$hour,$day,$month,$year,@rest) = @_;
+	$year += 1900;
+	$month += 1;  # !!!!!
+	$month = sprintf("%02d", $month);
+	$hour = sprintf("%02d", $hour);
+	$min = sprintf("%02d", $min);
+	$sec = sprintf("%02d", $sec);
+	return ($sec,$min,$hour,$day,$month,$year, @rest);
+}
+
+
+1;
--- lib/Utili/LogCmdtOO.pm
+++ lib/Utili/LogCmdtOO.pm
@@ -1,1 +1,149 @@
-/home/stof/09_code/cmdt_perl_tools/lib/Utili/LogCmdtOO.pm
+#!/usr/bin/perl
+=head1 NAME
+
+Utili::LogCmdtOO
+
+=item $LogCmdt::fhlog = log filehandler
+
+LogCmdt::logWrite((caller(0))[3], "start"); 
+>>>> (caller(0))[3] works only out from subroutine, not out from main!!!
+
+usage: initialize, open, write from main
+	our $jplog = Utili::LogCmdtOO->new(1, "../log/jplog.txt", 1, 0); #=item nil = new( writeLog, logFile, verboser, logAppend );
+	$jplog->logOpen();
+	$jplog->logWrite($config{tool}, "start..."); #=item nil = LogCmdt::logWrite( $caller, $logtext );
+and then:
+	$Jobparser::jplog->logWrite(( caller(0) )[3], "start testing...");
+
+fin:
+	$log->logWrite($config{tool}, "...end");
+	$log->logClose();
+	$log->logShowError();
+	
+=cut
+
+package Utili::LogCmdtOO;
+use warnings;
+use strict;
+use Data::Dumper;
+
+
+# __PACKAGE__
+# __SUB__ # nur ab bestimmter perl version
+# __LINE__
+# __FILE__
+# (caller(0))[3]
+#
+
+
+my $fhlog; 	#log file handler
+
+sub new {
+=item nil = new( writeLog, logFile, verboser, logAppend );
+creates instance
+#	my $writeLog	= shift; # 1=write log entry
+#	my $logFile	= shift;		##path/logfilename
+#	my $verboser	= shift; #1=output on screen
+#	my $logAppend	= shift; #1= append log file to existing
+=cut	
+	my $class = shift;
+	my $config = {
+		writeLog	=> shift, # 1=write log entry
+		logFile		=> shift,		##path/logfilename
+		verboser	=> shift, #1=output on screen
+		logAppend	=> shift, #1= append log file to existing
+	};
+
+	my $self = {};
+	bless $self, $class;	
+	$self->{config} = $config;
+	return $self;
+}	
+
+sub logOpen {
+=item nill = LogCmdt::logOpen();
+creates log file
+=cut
+	my $self = shift;
+	
+	if ($self->{config}->{writeLog}) {
+            if ($self->{config}->{logAppend}) {
+                    open $fhlog, ">>:encoding(UTF-8)", $self->{config}->{logFile} or die "$self->{config}->{logFile}: $!";
+            }
+            else {
+                open $fhlog, ">:encoding(UTF-8)", $self->{config}->{logFile} or die "$self->{config}->{logFile}: $!";
+            }
+#		$self->logWrite((caller(0))[3], "start");
+	}
+	return;
+
+}
+
+sub logInfo {
+	my $self = shift;
+	print "logInfo";
+	print Dumper $self; #logInfo$VAR1 = \bless( {}, 'Utili::Log' );
+	return;
+}
+
+
+
+sub logClose {
+=item $code = LogCmdt::logClose();
+close log file
+=cut
+	my $self = shift;
+	if ($self->{config}->{writeLog}) {
+#		$self->logWrite((caller(0))[3], "fin");
+		close $fhlog;
+	}
+	return;
+}
+
+sub logWrite {
+=item nil = LogCmdt::logWrite( $caller, $logtext );
+writes $logtext into logfile
+=cut
+	my $self = shift;
+	my $caller = shift;
+	my $logtext = shift;
+	my $str = localtime(time) . "\t $caller\t $logtext\n";
+	if ($self->{config}->{writeLog}) {
+		print $fhlog $str;	
+	}
+	if ($self->{config}->{verboser}) {
+		print STDERR "$str";		
+	}
+
+	return;
+}
+
+sub logShowError {
+	my $self = shift;
+	open($fhlog, $self->{config}->{logFile});
+	my @list=<$fhlog>;
+	my $searchFor="QS ERROR";
+	my @result=grep /$searchFor/,@list;
+	close $fhlog;
+	
+print "***********QS ERRORs************\n";	
+print Dumper @result;
+print "***********QS ERRORs fin************\n";	
+	return;
+}
+
+1;
+
+=head1 COPYRIGHT
+
+=for COPYRIGHT BEGIN
+
+cmdt.ch
+
+=for COPYRIGHT END
+
+=for LICENSE BEGIN
+
+CC-BY-SA cmdt L<http://cmdt.ch/>.
+
+=for LICENSE END
